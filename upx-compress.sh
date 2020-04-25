9be11073cddaf971b59c30624f2ea2d2121685d8
pexcn <i@pexcn.me>
2020-04-25 21:30:36 +0800

    add mips & mipsel version
    build with me, waiting official version.
    
    https://github.com/jpillora/chisel/issues/153

--- upx-compress.sh
+++ upx-compress.sh
@@ -3,5 +3,5 @@ set -o pipefail
 
 upx bin/arm/chisel_linux_armv7
 upx bin/aarch64/chisel_linux_arm64
-#upx --best bin/mips/chisel_linux_mips_sf
-#upx --best bin/mipsel/chisel_linux_mipsle_sf
+upx --best bin/mips/chisel_linux_mips_sf
+upx --best bin/mipsel/chisel_linux_mipsle_sf
