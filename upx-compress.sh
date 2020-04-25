7007a92253e51a3d0451b6bab6f5e4e145854b9b
pexcn <i@pexcn.me>
2020-04-25 20:25:59 +0800

    use upx default compression algorithm
    relate: https://github.com/openwrt-dev/openwrt-v2ray-plugin/issues/2

--- upx-compress.sh
+++ upx-compress.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 set -o pipefail
 
-upx --lzma bin/arm/chisel_linux_armv7
-upx --lzma bin/aarch64/chisel_linux_arm64
-#upx --lzma --best bin/mips/chisel_linux_mips_sf
-#upx --lzma --best bin/mipsel/chisel_linux_mipsle_sf
+upx bin/arm/chisel_linux_armv7
+upx bin/aarch64/chisel_linux_arm64
+#upx --best bin/mips/chisel_linux_mips_sf
+#upx --best bin/mipsel/chisel_linux_mipsle_sf
